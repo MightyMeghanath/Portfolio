601939c8fbe34153b4e49aa5f823c3f3c1fc9a54
G Meghanath <95625999+MightyMeghanath@users.noreply.github.com>
2023-11-02 21:02:13 +0530

    Add files via upload

---
+++ Portfilio/script.js
@@ -0,0 +1,7 @@
+console.log("Welcome to my new Project : PORTFOLIO")
+//Intialization
+let mai = document.querySelector('.main')
+// Click Events
+document.querySelector('.menu').addEventListener("click",()=>{
+    document.querySelector(".sidebar").classList.toggle("sidebaGO");
+})
